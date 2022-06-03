7d2340bce675abbd3f65bc52bdf686551fb861e3
behailu <behailuis4simachew@gmail.com>
2022-06-02 19:04:28 -0700

    Added the exact quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: Has to introduce another header library the <unistd.h>
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
