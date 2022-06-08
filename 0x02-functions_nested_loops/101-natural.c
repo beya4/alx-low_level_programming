aa2f4a993fb16ce7312dcce3d093ed5a7ad618e1
behailu <behailuis4simachew@gmail.com>
2022-06-08 11:14:47 -0700

    Added natural number less than 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of natural number below 1024, multiple of 3, 5
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+int i, sum = 0;
+
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum += i;
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
