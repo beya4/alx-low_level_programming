bc3f7d2214c293d50ad49770d50a4d738a27ca1a
behailu <behailuis4simachew@gmail.com>
2022-06-22 14:02:22 -0700

    Added palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome - check if a string is palindrome
+ * @s: char pointer
+ * Return: int
+ */
+int is_palindrome(char *s)
+{
+if (!*s)
+return (0);
+return (ch_palind(0, s));
+}
+/**
+ * ch_palind - check if a number is a palindrome
+ *
+ * @curri: int
+ * @s: char pointer
+ *
+ * Return: int
+ */
+
+int ch_palind(int curri, char *s)
+{
+int l = strlen(s)  - curri;
+int i = curri - 1;
+
+if (s[i] != s[l])
+return (0);
+if (l == 0 && s[i] == s[l])
+return (1);
+return (ch_palind(curri + 1, s));
+}
