f69e0a5bcb546dcef6fc939a8f488cc2ff1014af
behailu <behailuis4simachew@gmail.com>
2022-06-22 13:57:05 -0700

    Added prime identifier

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,13 @@
+int _putchar(int ch);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-void _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt(int n, int i);
-int wildcmp(char *s1, char *s2);
+int sqr(int n, int cv);
 int is_prime_number(int n);
+int prime(int n, int cv);
 int is_palindrome(char *s);
+int ch_palind(int curri, char *s);
+int wildcmp(char *s1, char *s2);
