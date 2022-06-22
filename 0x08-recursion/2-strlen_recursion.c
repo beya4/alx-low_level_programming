09e8c00a12dd1efc2213b4efb9c2a33b714f593d
behailu <behailuis4simachew@gmail.com>
2022-06-21 20:45:09 -0700

    added length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - will return the length of a string recursively
+ *
+ * @s: char pointer
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+int l = 0;
+if (*s)
+return (1 + _strlen_recursion(s + 1));
+return (l);
+}
