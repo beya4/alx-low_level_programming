7e1842d55ac72290db471642fa44782af0c61b2e
behailu <behailuis4simachew@gmail.com>
2022-06-24 13:13:49 -0700

    bug fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,58 @@
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * main -will print the sum of int command line argument pass
- * @argc: int
- * @argv: char pointef to pointer
- * Return: int
- */
-int main(int argc, char *argv[])
-{
-int i, sum = 0;
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-if (argc == 1)
+/**
+ * isNum - check if string array is num
+ * @num: string to check
+ * Return: 0 if it's a number
+ *         1 if it's not a number
+ */
+
+int isNum(char num[])
 {
-printf("0\n");
+int i, l = strlen(num);
+
+for (i = 0; i < l; i++)
+{
+if (!isdigit(num[i]))
+return (1);
+}
 return (0);
 }
 
+
+/**
+ * main - a program that adds positive numbers
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int i, sum;
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
+sum = 0;
 for (i = 1; i < argc; i++)
 {
-int currv = strtol(argv[i], NULL, 10);
-char *ch = "0";
-
-if (currv == 0 && *argv[i] != *ch)
+if (isNum(argv[i]) == 0)
+{
+sum += atoi(argv[i]);
+}
+else
 {
 printf("Error\n");
 return (1);
 }
-
-sum += currv;
 }
 printf("%d\n", sum);
-exit(EXIT_SUCCESS);
+}
+return (0);
 }
