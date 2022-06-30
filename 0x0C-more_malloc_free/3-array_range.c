2528732127c6918b0b557c741c531074d49b34ce
behailu <behailuis4simachew@gmail.com>
2022-06-30 10:26:23 -0700

    bug fixedd

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,24 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * _calloc - allocates memory for an array, using malloc.
- * @nmemb: number of elements in the array
- * @size: size in bytes of the elements
+ * array_range - initializes memory spaces with zero.
+ * @min: string 1.
+ * @max: string 2, concatenated to 1
  *
- * Return: void pointer to allocated memory
+ * Return: pointer to the concatenated string.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+int *array_range(int min, int max)
 {
-char *p;
-unsigned int i;
+int *newArray;
+int i, length;
 
-if (nmemb == 0 || size == 0)
+if (min > max)
 return (NULL);
-p = malloc(nmemb * size);
-if (p == NULL)
+
+length = max - min + 1;
+newArray = malloc(length *sizeof(int));
+if (newArray == NULL)
 return (NULL);
-for (i = 0; i < (nmemb * size); i++)
-p[i] = 0;
-return (p);
+
+for (i = 0; i < length; i++)
+{
+*(newArray + i) = min;
+min++;
+}
+
+return (newArray);
 }
