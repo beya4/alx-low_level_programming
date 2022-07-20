abdfe16cdf490b030302d01416a089a967ada09c
behailu <behailuis4simachew@gmail.com>
2022-07-19 17:49:36 -0700

    free listint safe

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint_safe - will print all nodes of a linkedlist
+ * @h: head of the list
+ * Return: number of node printed
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+size_t len = 0;
+
+if (h == NULL)
+
+return (len);
+while (*h != NULL)
+{
+listint_t *tmp;
+
+len++;
+if (*h > (*h)->next)
+{
+tmp = *h;
+*h = (*h)->next;
+free(tmp);
+}
+else
+{
+free(*h);
+*h = NULL;
+}
+}
+*h = NULL;
+
+return (len);
+}
